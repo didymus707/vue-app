778a4e09449ebae8380bba77baf750afe4f4fdb7
Adewale Orotayo <didymus7007@gmail.com>
2023-08-08 12:17:48 +0100

    data and one way data binding

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Moustache syntax wraps the data property in double curly braces.</p>
+  <p v-pre>Example: {{ name }}</p>
+
+  <p>The resulting output will be the value of the data property.</p>
+  <p>Example: {{ name }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: 'John Doe'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.top {
+  display: flex;
+  width: 100%;
+}
+.btn {
+  margin-left: 8px;
+}
+.valid {
+  color: forestgreen;
+}
+.invalid {
+  color: crimson;
+}
+.text-22 {
+  font-size: 16px;
+}
+.bold {
+  font-weight: bold;
+}
+.italic {
+  font-style: italic;
 }
 </style>
