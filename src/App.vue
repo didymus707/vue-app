239f77f32ba5e452c5ef8dfe28e029e7a5db6d02
Adewale Orotayo <didymus7007@gmail.com>
2023-08-10 15:24:58 +0100

    iteration rendering

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,27 @@
 <template>
-  <div class="top">
-    <p>Counter: {{ count }}</p>
-
-    <button @click="count++">Increment</button>
-    <button @click="count--">Decrement</button>
-  </div>
+  <template v-for="user in filter" :key="user.name">
+    <p>{{ user.name }} is at the age where he can vote</p>
+  </template>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 0,
+      users: [
+        { name: "De", age: 33 },
+        { name: "Andre", age: 34 },
+        { name: "Yedlin", age: 29 },
+        { name: "Stefan", age: 18 },
+        { name: "Tanitoluwa", age: 7 },
+        { name: "Toluwalase", age: 7 },
+      ],
     };
   },
   computed: {
-    fullName: {
-      get() {
-        return this.firstName + " " + this.lastName;
-      },
-      set(value) {
-        const names = value.split(" ");
-        this.firstName = names[0];
-        this.lastName = names[1];
-      },
-    },
-  },
-  watch: {
-    count(newValue, oldValue) {
-      if (newValue > oldValue && newValue === 5)
-        alert("Increasing the volume past 5 may damage your hearing");
+    filter() {
+      return this.users.filter((user) => user.age >= 18);
     },
   },
 };
 </script>
-
-<style scoped>
-.top {
-  width: 40%;
-  display: flex;
-  flex-direction: column;
-  color: forestgreen;
-}
-.margin {
-  margin-top: 8px;
-}
-.input {
-  width: 28%;
-  padding: 6px 12px;
-  border-radius: 4px;
-  border: 1px solid forestgreen;
-}
-</style>
