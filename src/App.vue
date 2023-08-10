a4af08d9a37633bd6f4ef1447562f0de92b28fc5
Adewale Orotayo <didymus7007@gmail.com>
2023-08-10 12:28:32 +0100

    two-way databinding with v-model

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="top">
-    <p>Your name: {{ name }}</p>
-    <input type="text" @input="getInput($event, 'Johnson')" />
-    <!-- lock this counter after first render -->
-    <p v-once>Initial value: {{ counter }}</p>
-    <p>Updated value: {{ counter }}</p>
-
-    <button @click="increment">Increment</button>
-    <button @click="decrement">Decrement</button>
+    <h1>Full Name: {{ firstName }} {{ lastName }}</h1>
+    <label class="margin">First Name: {{ firstName }}</label>
+    <input class="margin input" type="text" v-model="firstName" />
+    <label class="margin">Last Name: {{ lastName }}</label>
+    <input class="margin input" type="text" v-model="lastName" />
   </div>
 </template>
 
@@ -15,27 +12,28 @@
 export default {
   data() {
     return {
-      name: "",
-      counter: 10,
+      firstName: "",
+      lastName: "",
     };
   },
-  methods: {
-    getInput(event, lastName) {
-      this.name = event.target.value + " " + lastName;
-    },
-    increment() {
-      this.counter++;
-    },
-    decrement() {
-      this.counter--;
-    },
-  },
+  
 };
 </script>
 
 <style scoped>
 .top {
+  width: 40%;
+  display: flex;
+  flex-direction: column;
   color: forestgreen;
 }
+.margin {
+  margin-top: 8px;
+}
+.input {
+  width: 28%;
+  padding: 6px 12px;
+  border-radius: 4px;
+  border: 1px solid forestgreen;
+}
 </style>
->
