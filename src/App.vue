f4c0f9a1398fad4f87c51f2bedfd5a603fe2ee6b
Adewale Orotayo <didymus7007@gmail.com>
2023-09-12 15:38:46 +0100

    provide and inject

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <script>
-import GreetingMessage from "./components/GreetingMessage";
+import HelloWorld from './components/HelloWorld';
 import ChildGreeting from "./components/ChildGreeting";
+import GreetingMessage from "./components/GreetingMessage";
 export default {
   components: {
     GreetingMessage,
     ChildGreeting,
+    HelloWorld,
   },
   computed: {
     filter() {
       return this.users.filter((user) => user.age >= 18);
     },
   },
+  provide: {
+    firstName: 'Scott',
+    lastName: 'Lang'
+  }
 };
 </script>
 
 <template>
   <greeting-message />
-  <child-greeting firstName="Scott" lastName="Lang" />
+  <hello-world firstName="Scott" lastName="Lang" />
   <child-greeting firstName="Peter" lastName="Parker" />
   <child-greeting firstName="Tony" lastName="Stark" />
   <child-greeting firstName="Thor" lastName="Son of Odin" />
